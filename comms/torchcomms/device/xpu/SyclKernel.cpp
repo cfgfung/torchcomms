d996edd0e4695cfe50d0859b9e25f25e2540c650
Raymond Lau <cfgfung@gmail.com>
2026-04-02 12:59:49 -0700

    Align with the XCCL version.

---
+++ comms/torchcomms/device/xpu/SyclKernel.cpp
@@ -0,0 +1,24 @@
+#include <sycl/sycl.hpp>
+#include <ATen/ATen.h>
+#include "comms/torchcomms/device/xpu/XpuApi.hpp"
+
+namespace torch::comms {
+    int CloneAndMulKernel(const xpuStream_t &stream, at::Tensor &src, at::Tensor &dst, const float &factor){
+        try {
+            sycl::queue q = stream.queue();
+            auto* src_data = src.data_ptr<float>();
+            auto* dst_data = dst.data_ptr<float>();
+            int64_t numel = src.numel();
+            q.submit([&](sycl::handler& h) {
+                h.parallel_for(sycl::range<1>(numel), [=](sycl::id<1> i) {
+                    dst_data[i] = src_data[i] * factor; 
+                });
+            });
+        } catch (sycl::exception const& e) {
+            std::cerr << "SYCL exception caught: " << e.what() << "\n";
+            return 1;
+        }
+
+        return 0;
+    }
+} // namespace torch::comms
---
+++ comms/torchcomms/device/xpu/SyclKernel.hpp
@@ -0,0 +1,11 @@
+
+#pragma once
+#include <sycl/sycl.hpp>
+#include <ATen/ATen.h>
+#include "comms/torchcomms/device/xpu/XpuApi.hpp"
+
+namespace torch::comms {
+    // Create a new tensor -- at::empty_like, uninitialized
+    // Read the memory and multiply it into the new tensor
+    int CloneAndMulKernel(const xpuStream_t &stream, at::Tensor &src, at::Tensor &dst, const float &factor);
+} // namespace torch::comms
